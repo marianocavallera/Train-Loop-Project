67c31fb53df51a63542aa3959ba4ef644f569965
Mariano Angel Cavallera <mcavallera99@gmail.com>
2022-10-22 17:00:56 -0300

    v1.0
    sprites (player/escenario)
    scripts (movimiento)

---
+++ Assets/Scripts/GotoMouse.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GotoMouse : MonoBehaviour
+{
+
+    public float speed = 1.5f;
+    private Vector3 target;
+
+    void Start()
+    {
+        target = transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+}
