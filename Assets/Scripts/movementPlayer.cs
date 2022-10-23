3873502b489083ac166b35bb80e9596e6b1350c9
jpasdfff12 <jprattalino7@gmail.com>
2022-10-23 12:58:54 -0300

    animations and script

--- Assets/Scripts/movementPlayer.cs
+++ Assets/Scripts/movementPlayer.cs
@@ -7,12 +7,14 @@ public class movementPlayer : MonoBehaviour
 
     [SerializeField] private float speed = 3f;
     private Rigidbody2D playeRb;
+    private Animator Animator;
     private Vector2 moveInput;
 
     // Start is called before the first frame update
     void Start()
     {
         playeRb = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,6 +23,9 @@ public class movementPlayer : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         moveInput = new Vector2(moveX, moveY).normalized;
+        
+        Animator.SetBool("Running", moveX != 0.0f);
+        Animator.SetBool("Running", moveY != 0.0f);
     }
 
     private void FixedUpdate()
