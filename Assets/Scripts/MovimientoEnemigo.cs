f7b268e265a995d6bd9592d4e475d0321a8bc433
jpasdfff12 <jprattalino7@gmail.com>
2022-10-26 10:52:36 -0300

    Update MovimientoEnemigo.cs
    -ahora te sigue si la dist es menor a X;

--- Assets/Scripts/MovimientoEnemigo.cs
+++ Assets/Scripts/MovimientoEnemigo.cs
@@ -24,8 +24,9 @@ public class MovimientoEnemigo : MonoBehaviour
 
         float angulo = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-
-        transform.position = Vector2.MoveTowards(this.transform.position, Jugador.transform.position, speedenemigo * Time.fixedDeltaTime);
-        transform.rotation = Quaternion.Euler(Vector3.forward * angulo);
+        if(distancia < 2){
+            transform.position = Vector2.MoveTowards(this.transform.position, Jugador.transform.position, speedenemigo * Time.fixedDeltaTime);
+            transform.rotation = Quaternion.Euler(Vector3.forward * angulo);
+        }
     }
 }
