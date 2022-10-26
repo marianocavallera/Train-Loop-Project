4f7ec184bcd6d6865fdc4caeb05b498417919033
jpasdfff12 <jprattalino7@gmail.com>
2022-10-26 10:47:29 -0300

    enemy function
    add script

---
+++ Assets/Scripts/MovimientoEnemigo.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoEnemigo : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float distancia;
+    public float speedenemigo;
+    public GameObject Jugador;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distancia = Vector2.Distance(transform.position, Jugador.transform.position);
+        Vector2 direction = Jugador.transform.position - transform.position;
+        direction.Normalize();
+
+        float angulo = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+
+        transform.position = Vector2.MoveTowards(this.transform.position, Jugador.transform.position, speedenemigo * Time.fixedDeltaTime);
+        transform.rotation = Quaternion.Euler(Vector3.forward * angulo);
+    }
+}
